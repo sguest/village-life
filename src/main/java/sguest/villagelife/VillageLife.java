907102b62b77eb18d949ccd9107a30f8fced00c2
sguest <spencer.b.guest@gmail.com>
2020-10-29 13:19:32 -0500

    Meta stuff - loads as a recognized mod

---
+++ src/main/java/sguest/villagelife/VillageLife.java
@@ -0,0 +1,10 @@
+package sguest.villagelife;
+
+import net.minecraftforge.fml.common.Mod;
+
+import static sguest.villagelife.VillageLife.MOD_ID;
+
+@Mod(MOD_ID)
+public class VillageLife {
+    public static final String MOD_ID = "villagelife";
+}
