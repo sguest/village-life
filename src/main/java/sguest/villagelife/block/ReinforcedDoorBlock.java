c20dbd4272ac609ec021fa981b197f8a1391154b
sguest <spencer.b.guest@gmail.com>
2020-11-12 18:27:28 -0600

    Import cleanup

--- src/main/java/sguest/villagelife/block/ReinforcedDoorBlock.java
+++ src/main/java/sguest/villagelife/block/ReinforcedDoorBlock.java
@@ -10,7 +10,6 @@ import net.minecraft.entity.monster.ZombieEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.IBlockReader;
