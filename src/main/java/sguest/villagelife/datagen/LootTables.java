d1aba85a6e82ca5e2f597b795601258a3f7c9b18
sguest <spencer.b.guest@gmail.com>
2020-11-20 16:08:49 -0600

    Move injected chest loot tables to datagen

--- src/main/java/sguest/villagelife/datagen/LootTables.java
+++ src/main/java/sguest/villagelife/datagen/LootTables.java
@@ -3,11 +3,14 @@ package sguest.villagelife.datagen;
 import net.minecraft.block.Block;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.item.Items;
+import net.minecraft.loot.EmptyLootEntry;
+import net.minecraft.loot.ItemLootEntry;
 import net.minecraft.loot.functions.CopyNbt;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraftforge.fml.RegistryObject;
 import sguest.villagelife.block.ModBlocks;
 import sguest.villagelife.entity.merchant.villager.ModProfessions;
+import sguest.villagelife.item.ModItems;
 
 public class LootTables extends BaseLootTableProvider {
     public LootTables(DataGenerator dataGeneratorIn) {
@@ -36,6 +39,35 @@ public class LootTables extends BaseLootTableProvider {
             Items.OAK_LOG,
             Items.SPRUCE_LOG);
         addHeroGift(ModProfessions.INNKEEPER.get(), Items.MUSHROOM_STEM, Items.RABBIT_STEW, Items.BEETROOT_SOUP);
+
+        addInjectTable("chests/village/village_weaponsmith", builder -> {
+            builder.addEntry(ItemLootEntry.builder(ModItems.EMERALD_HORSE_ARMOR.get()).weight(10));
+            builder.addEntry(EmptyLootEntry.func_216167_a().weight(90));
+        });
+        addInjectTable("chests/desert_pyramid", builder -> {
+            builder.addEntry(ItemLootEntry.builder(ModItems.EMERALD_HORSE_ARMOR.get()).weight(11));
+            builder.addEntry(EmptyLootEntry.func_216167_a().weight(89));
+        });
+        addInjectTable("chests/end_city_treasure", builder -> {
+            builder.addEntry(ItemLootEntry.builder(ModItems.EMERALD_HORSE_ARMOR.get()).weight(4));
+            builder.addEntry(EmptyLootEntry.func_216167_a().weight(96));
+        });
+        addInjectTable("chests/jungle_temple", builder -> {
+            builder.addEntry(ItemLootEntry.builder(ModItems.EMERALD_HORSE_ARMOR.get()).weight(4));
+            builder.addEntry(EmptyLootEntry.func_216167_a().weight(96));
+        });
+        addInjectTable("chests/nether_bridge", builder -> {
+            builder.addEntry(ItemLootEntry.builder(ModItems.EMERALD_HORSE_ARMOR.get()).weight(20));
+            builder.addEntry(EmptyLootEntry.func_216167_a().weight(80));
+        });
+        addInjectTable("chests/simple_dungeon", builder -> {
+            builder.addEntry(ItemLootEntry.builder(ModItems.EMERALD_HORSE_ARMOR.get()).weight(12));
+            builder.addEntry(EmptyLootEntry.func_216167_a().weight(88));
+        });
+        addInjectTable("chests/stronghold_corridor", builder -> {
+            builder.addEntry(ItemLootEntry.builder(ModItems.EMERALD_HORSE_ARMOR.get()).weight(3));
+            builder.addEntry(EmptyLootEntry.func_216167_a().weight(97));
+        });
     }
 
     private void addDoor(RegistryObject<Block> door) {
