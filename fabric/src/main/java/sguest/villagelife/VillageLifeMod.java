d80d33b05362bf6e487376aa55a027226ae16fc0
sguest <spencer.b.guest@gmail.com>
2021-11-07 17:53:29 -0600

    Mod ID and namespace

--- fabric/src/main/java/sguest/villagelife/VillageLifeMod.java
+++ fabric/src/main/java/sguest/villagelife/VillageLifeMod.java
@@ -1,14 +1,15 @@
-package net.fabricmc.example;
+package sguest.villagelife;
 
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class ExampleMod implements ModInitializer {
+public class VillageLifeMod implements ModInitializer {
+	public static final String MODID = "villagelife";
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger("modid");
+	public static final Logger LOGGER = LogManager.getLogger(MODID);
 
 	@Override
 	public void onInitialize() {
